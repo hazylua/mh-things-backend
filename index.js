034e940f1a57ac5d92af50c9d557bb034acd7bca
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-23 22:07:51 -0300

    Use cors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 
 const express = require("express"),
   routes = require("./routes"),
+  cors = require("cors"),
   port = process.env.PORT,
   server = express();
+
+server.use(cors());
 
 server.use("/data", routes.data);
 server.listen(port, () => console.log(`Listening on port ${port}.`));
